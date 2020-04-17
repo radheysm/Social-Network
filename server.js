f244595647491e50eb84d3718a25668a110f8e2c
radheysm <radheysaraan@gmail.com>
2020-04-17 16:06:05 +0530

    mongodb connector called

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+
+// Connect Database
+connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
 
@@ -10,3 +14,7 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=>{
     console.log(`server start on port ${PORT}`);
 })
+
+
+
+
