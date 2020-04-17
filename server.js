9f539c7ffd2e32a0d6fe8e3deba583b8123e450c
radheysm <radheysaraan@gmail.com>
2020-04-17 11:32:09 +0530

    setup the express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5001;
+
+
+app.listen(PORT, ()=>{
+    console.log(`server start on port ${PORT}`);
+})
