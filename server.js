b352c5f13960fa2c873f90e086235d78bbfdc7cb
radheysm <radheysaraan@gmail.com>
2020-04-17 17:27:30 +0530

    modified

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+
+app.use(express.json({exteded:false}));
+
+
 app.get('/', (req,res) => res.send('API Running'));
+
+
+// Define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5001;
 
