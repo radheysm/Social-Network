81375944b10c55c96490a09a7c66277ab19fa592
radheysm <radheysaraan@gmail.com>
2020-04-19 17:04:44 +0530

    for post created

---
+++ routes/api/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route     GET api/post
+// @desc      Test route
+// @access    Public
+
+router.get('/', (req, res)=> res.send("Post routes"));
+
+
+
+
+module.exports = router;
