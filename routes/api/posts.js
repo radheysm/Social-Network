5ea06aa451e0ec3401c40cbe4dfdc7de1970a737
radheysm <radheysaraan@gmail.com>
2020-04-20 23:23:12 +0530

    Get post by Id and Delete post by ID API request created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -53,5 +53,46 @@ router.get('/', auth, async (req, res) =>{
     }
 });
 
+// @route     GET api/post/:id
+// @desc      Get post by id
+// @access    Private
+
+router.get('/:id', auth, async (req, res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({msg:"Post not find"});
+        }
+        res.send(post);
+
+
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg:"Post not find"});
+        }
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route     DELETE api/post/:id
+// @desc      Delete a Post 
+// @access    Private
+
+router.delete('/:id', auth, async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        // Check user
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg:"User not Authorized"});
+        }
+        await post.remove();
+        res.json({msg:"Post Removed"});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 
 module.exports = router;
