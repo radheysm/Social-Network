75a95dfa4fe58f60ba645805ca18f52a674ea94e
radheysm <radheysaraan@gmail.com>
2020-04-21 16:29:00 +0530

    Authorization added for sending data throuh header

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ connectDB();
 
 app.use(express.json({exteded:false}));
 
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
 app.get('/', (req,res) => res.send('API Running'));
 
 
