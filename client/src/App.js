59d94adf2df8b7d1c581d05595dd5e5ea8f61872
radheysm <radheysaraan@gmail.com>
2020-04-21 12:54:53 +0530

    Update Router and Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 const App = () => 
-
-    <Fragment>
-     <Navbar/>
-     <Landing/>
-    </Fragment>
-
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route path = '/' component = {Landing}/>
+      </Fragment>
+    </Router>
 
 
 export default App;
