c8dcca770b3123d68e03ba13096d965378055f56
radheysm <radheysaraan@gmail.com>
2020-04-21 16:29:53 +0530

    Register form is updated data recieved and updated using useState

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
-
+// import axios from 'axios';
 const Register = () =>{
     const [formData, setFormData] = useState({
         name:'',
@@ -13,13 +13,30 @@ const Register = () =>{
 
     const onChange = e => setFormData({...formData, [e.target.name]:e.target.value});
     
-    const onSubmit = e => {
+    const onSubmit = async e => {
        e.preventDefault();
        if(password !== password2){
            console.log("Password do not match");
        }
        else{
-           console.log(formData);
+           console.log("success");
+        //    const newUser = {
+        //        name,
+        //        email,
+        //        password
+        //    }
+        //    try {
+        //        const config = {
+        //            header:{
+        //                'Content-Type':'application/json'
+        //            }
+        //        }
+        //        const body = JSON.stringify(newUser);
+        //        const res = await axios.post('http://localhost:5001/api/users', body, config);
+        //        console.log(res.data);
+        //    } catch (err) {
+        //        console.error(err.response.data);
+        //    }
        }
     };
 
@@ -70,7 +87,7 @@ const Register = () =>{
             minLength="6"
             value={password2} 
             onChange = {e => onChange(e)}
-            requiredgit
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
