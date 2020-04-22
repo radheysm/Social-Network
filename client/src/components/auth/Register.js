412449724e4dcc1625e02f89af0754c434b683e2
radheysm <radheysaraan@gmail.com>
2020-04-22 16:50:16 +0530

    called register action when user successfully registered

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 // import axios from 'axios';
-const Register = ({setAlert}) =>{
+const Register = ({setAlert, register}) =>{
     const [formData, setFormData] = useState({
         name:'',
         email:'',
@@ -23,7 +24,7 @@ const Register = ({setAlert}) =>{
            setAlert("Password do not match", 'danger');
        }
        else{
-           console.log("success");
+           register({name, email, password});
         //    const newUser = {
         //        name,
         //        email,
@@ -106,8 +107,9 @@ const Register = ({setAlert}) =>{
 };
 
 Register.propTypes = {
-    setAlert:PropTypes.func.isRequired
+    setAlert:PropTypes.func.isRequired,
+    register:PropTypes.func.isRequired
 }
 
 
-export default connect(null, {setAlert})(Register);
+export default connect(null, {setAlert, register})(Register);
