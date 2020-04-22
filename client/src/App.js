73b8b740619c2a3c08b27d2af07e18a36d1c9751
radheysm <radheysaraan@gmail.com>
2020-04-22 15:29:26 +0530

    Alert component added to routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,23 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-const App = () => 
+import Alert from './components/layout/Alert';
+
+// Redux
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+
+
+const App = () => (
+<Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
         <Route exact path = '/' component = {Landing}/>
         <section className = "container">
+          <Alert/>
           <Switch>
             <Route exact path = '/register' component = {Register}/>
             <Route exact path = '/login' component = {Login}/>
@@ -18,6 +29,6 @@ const App = () =>
         </section>
       </Fragment>
     </Router>
-
-
+    </Provider>
+);
 export default App;
