ab83ef929611dd3309714b0e8d79d1a4eb08d79c
radheysm <radheysaraan@gmail.com>
2020-04-24 14:38:18 +0530

    logout action created

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from './types';
 
 import setAuthToken from '../utills/setAuthToken';
@@ -90,3 +91,9 @@ export const login = (email, password) => async dispatch => {
         });
     }
 };
+
+// Logout
+
+export const logout = () => dispatch => {
+    dispatch({type:LOGOUT})
+}
