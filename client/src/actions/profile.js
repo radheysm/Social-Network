4f099509cdaf80b77f9d94f017a1536d0e733d62
radheysm <radheysaraan@gmail.com>
2020-04-24 21:39:20 +0530

    Create-profile action created

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -25,3 +25,37 @@ export const getCurrentProfile = () => async dispatch =>{
         });
     }
 }
+
+// Create or Update profile 
+
+export const createProfile = (formData, history, edit=false) => async dispatch => {
+    try {
+
+        const config = {
+            headers:{
+                'Content-type':'application/json'
+            }
+        }
+        // const body = JSON.stringify(formData);
+        const res = await axios.post('http://localhost:5001/api/profile',formData, config);
+        dispatch({
+            type:GET_PROFILE,
+            payload:res.data
+        });
+        dispatch(setAlert(edit? 'Profile Updated' : 'Profile Created','success'));
+        if(!edit){
+            history.push('/dashboard');
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type:PROFILE_ERROR,
+            payload:{msg:err.response.statusText, status:err.response.status}
+
+        });
+    }
+}
+
