cfba45fccc640af5cb21832f9869cdebf1f2b742
radheysm <radheysaraan@gmail.com>
2020-04-24 14:16:01 +0530

    login and loaduser action added

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,8 +2,35 @@ import axios from 'axios';
 import {setAlert} from './alert';
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from './types';
+
+import setAuthToken from '../utills/setAuthToken';
+
+// Load User
+
+export const loadUser = () => async dispatch =>{
+    if(localStorage.token){
+         setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('http://localhost:5001/api/auth');
+        dispatch({
+            type:USER_LOADED,
+            payload:res.data
+        });
+    } catch (err) {
+        dispatch({
+            type:AUTH_ERROR
+        });
+    }
+}
+
 
 // Register User
 
@@ -22,7 +49,8 @@ export const register = ({name, email, password}) => async dispatch =>{
         dispatch({
             type: REGISTER_SUCCESS,
             payload:res.data
-        })
+        });
+        dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
         if(errors){
@@ -35,3 +63,30 @@ export const register = ({name, email, password}) => async dispatch =>{
 
 };
 
+// Login User
+
+export const login = (email, password) => async dispatch => {
+
+    const config = {
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    const body = JSON.stringify({email, password});
+    try {
+        const res = await axios.post('http://localhost:5001/api/auth', body, config);
+        dispatch({
+            type:LOGIN_SUCCESS,
+            payload:res.data
+        });
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type:LOGIN_FAIL
+        });
+    }
+};
