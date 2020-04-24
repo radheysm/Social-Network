9715190cd3114ec1a1e9ab01434aba58ac47310a
radheysm <radheysaraan@gmail.com>
2020-04-24 21:37:49 +0530

    Create-profile route added

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-form/CreateProfile';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -36,6 +37,7 @@ const App = () => {
             <Route exact path = '/register' component = {Register}/>
             <Route exact path = '/login' component = {Login}/>
             <PrivateRoute exact path = '/dashboard' component = {Dashboard}/>
+            <PrivateRoute exact path = '/create-profile' component = {CreateProfile}/>
           </Switch>
         </section>
       </Fragment>
