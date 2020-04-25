14c66d464653cf47cdb3021ea68818a54be21e45
radheysm <radheysaraan@gmail.com>
2020-04-25 10:44:08 +0530

    Edit Profile route added

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import store from './store';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
+import EditProfile from './components/profile-form/EditProfile';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -38,6 +39,7 @@ const App = () => {
             <Route exact path = '/login' component = {Login}/>
             <PrivateRoute exact path = '/dashboard' component = {Dashboard}/>
             <PrivateRoute exact path = '/create-profile' component = {CreateProfile}/>
+            <PrivateRoute exact path = '/edit-profile' component = {EditProfile}/>
           </Switch>
         </section>
       </Fragment>
