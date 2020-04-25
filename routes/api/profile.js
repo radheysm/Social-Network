8627cbd503c0f2cd973dee819cc1e075b0d816e5
radheysm <radheysaraan@gmail.com>
2020-04-25 10:45:45 +0530

    split() function handled error resolved split is not a function

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,7 +58,7 @@ router.post('/', [
         if(status) profileFields.status = status;
         if(githubusername) profileFields.githubusername = githubusername;
         if(skills){
-            profileFields.skills = skills.split(',').map(skill => skill.trim());
+            profileFields.skills = skills.toString().split(',').map(skill => skill.trim());
 
         }
 
