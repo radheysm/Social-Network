d823ea5aa0cae4e9199d166a2d8c135c3de59099
radheysm <radheysaraan@gmail.com>
2020-04-25 13:51:05 +0530

    /add-experience and /add-education route added

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -40,6 +42,8 @@ const App = () => {
             <PrivateRoute exact path = '/dashboard' component = {Dashboard}/>
             <PrivateRoute exact path = '/create-profile' component = {CreateProfile}/>
             <PrivateRoute exact path = '/edit-profile' component = {EditProfile}/>
+            <PrivateRoute exact path = '/add-experience' component = {AddExperience}/>
+            <PrivateRoute exact path = '/add-education' component = {AddEducation}/>
           </Switch>
         </section>
       </Fragment>
