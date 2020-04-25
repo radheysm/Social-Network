f52995f4c385f6aeed9e5f77c36f66b3486ee3f8
radheysm <radheysaraan@gmail.com>
2020-04-25 11:12:31 +0530

    Add experience and education to profile action created

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import {setAlert} from './alert';
 
 import {
     GET_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_PROFILE
 } from './types';
 
 // Get Current users profile
@@ -59,5 +60,75 @@ export const createProfile = (formData, history, edit=false) => async dispatch =
 
         });
     }
-}
+};
+
+
+// Add Experience
+
+export const addExperience = (fromData, history) => async dispatch =>{
+
+    try {
+        const config = {
+            headers:{
+                'Content-Type':'application/json'
+            }
+        };
+        const res = await axios.put('http://localhost:5001/api/profile/experience', formData, config);
+        
+        dispatch({
+            type:UPDATE_PROFILE,
+            payload:res.data
+        });
+        dispatch(setAlert('Experience Added', 'success'));
+        history.push('/dashboard');
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type:PROFILE_ERROR,
+            payload:{msg:err.response.statusText, status:err.response.status}
+        });
+        
+    }
+
+};
+
+
+// Add Education
+
+export const addEducation = (formData, history) => dispatch =>{
+
+    try {
+        const config = {
+            header:{
+                'Content-Type':'application/json'
+            }
+        };
+
+        const res = await axios.put('http://localhost:5001/api/profile/education', formData, config);
+        dispatch({
+            type:UPDATE_PROFILE,
+            payload:res.data
+        });
+
+        dispatch(setAlert('Education Added', 'success'));
+        history.push('/dashboard');
+
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if(errors){
+            errors.forEach(error => setAlert(error.msg,'danger'));
+        }
+        dispatch({
+            type:PROFILE_ERROR,
+            payload:{msg:err.response.statusText, status:err.response.status}
+        });
+    }
+};
+
 
