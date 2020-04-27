aa3e43394c6bbbe2def6c04edebc69994b6581d5
radheysm <radheysaraan@gmail.com>
2020-04-27 18:51:20 +0530

    addLike and removeLike action created

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {setAlert} from './alert';
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
 } from './types';
 
 
@@ -14,6 +15,42 @@ export const getPosts = () => async dispatch => {
         dispatch({
             type:GET_POSTS,
             payload:res.data
+        });
+    } catch (err) {
+        dispatch({
+            type:POST_ERROR,
+            payload:{msg:err.response.statusText, status:err.response.status}
+
+        });
+    }
+}
+
+// Add Like
+
+export const addLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`http://localhost:5001/api/post/like/${postId}`);
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{postId, likes:res.data}
+        });
+    } catch (err) {
+        dispatch({
+            type:POST_ERROR,
+            payload:{msg:err.response.statusText, status:err.response.status}
+
+        });
+    }
+}
+
+// remove Like
+
+export const removeLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`http://localhost:5001/api/post/unlike/${postId}`);
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{postId, likes:res.data}
         });
     } catch (err) {
         dispatch({
