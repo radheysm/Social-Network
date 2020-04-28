70dab0c1118e4471b7b84641b81c0c263027dd7e
radheysm <radheysaraan@gmail.com>
2020-04-28 10:46:17 +0530

    GET_POST case handled

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,7 +3,8 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
+    ADD_POST,
+    GET_POST
 } from '../actions/types';
 
 
@@ -27,6 +28,12 @@ export default function(state = initialState, action){
               return {
                   ...state,
                   posts:[payload, ...state.posts],
+                  loading:false
+              }
+          case GET_POST:
+              return{
+                  ...state,
+                  post:payload,
                   loading:false
               }
           case DELETE_POST:
