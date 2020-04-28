ac3284a4416828522257e35ee5c6dfa4ef7ab486
radheysm <radheysaraan@gmail.com>
2020-04-28 16:05:45 +0530

    warning resolved

--- client/src/components/profille/ProfileTop.js
+++ client/src/components/profille/ProfileTop.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const ProfileTop = ({profile:{
@@ -24,42 +24,42 @@ const ProfileTop = ({profile:{
           <div className="icons my-1">
           {
         website && (
-            <a href={website} target='_blank' rel='noopener noreferre'>
+            <a href={website} target='_blank' rel="noopener noreferrer">
               <i className="fas fa-globe fa-2x"></i>
             </a>
         )
     }   
     {
       social && social.twitter && (
-        <a href={social.twitter} target='_blank' rel='noopener noreferre'>
+        <a href={social.twitter} target='_blank' rel="noopener noreferrer">
         <i className="fab fa-twitter fa-2x"></i>
       </a>
       )   
     }    
     {
         social && social.facebook && (
-            <a href={social.facebook} target='_blank' rel='noopener noreferre'>
+            <a href={social.facebook} target='_blank' rel="noopener noreferrer">
             <i className="fab fa-facebook fa-2x"></i>
           </a>
         )
     } 
     {
         social && social.linkedin && (
-<a href={social.linkedin} target='_blank' rel='noopener noreferre'>
+<a href={social.linkedin} target='_blank' rel="noopener noreferrer">
               <i className="fab fa-linkedin fa-2x"></i>
             </a>
         )
     }
     {
         social && social.youtube && (
-            <a href={social.youtube} target='_blank' rel='noopener noreferre'>
+            <a href={social.youtube} target='_blank' rel="noopener noreferrer">
             <i className="fab fa-youtube fa-2x"></i>
           </a>
         )
     }
     {
         social && social.instagram && (
-<a href={social.instagram} target='_blank' rel='noopener noreferre'>
+<a href={social.instagram} target='_blank' rel="noopener noreferrer">
               <i className="fab fa-instagram fa-2x"></i>
             </a>
         )
