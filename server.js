0a8eab3008cc71954802c419bd78473e991f817b
radheysm <radheysaraan@gmail.com>
2020-05-06 15:34:19 +0530

    modified static folder updated uploads added as static folder using express.static

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/posts'));
 
+app.use('/uploads',express.static('uploads'));
+
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
@@ -45,6 +47,7 @@ if(process.env.NODE_ENV === 'production'){
    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
   });
 }
+
 
 const PORT = process.env.PORT || 5001;
 
